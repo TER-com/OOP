5e9e1390ef436506152d893d6c80f3a927eb3d12
https://ter <esthergbblesa@gmail.com>
2026-02-26 12:58:11 +0100

    saludo

---
+++ prueba2.py
@@ -0,0 +1,7 @@
+#SIEMPRE SE USAN ESTOS TRES COMANDOS EN LA TERMINAL
+
+#git add -A #define la nueva versión; creas el paquete
+#git commit -m "comentario" #guarda el codigo con el mensaje explicativo; le pones para donde va el paquete
+#git push #subir el codigo; ir a correos y dejar el paquete
+
+print("Hola caracola puchinbola")
