c7f3647d3f036b8baacfb7c8bac82652267d073a
https://ter <esthergbblesa@gmail.com>
2026-02-27 19:39:54 +0100

    ejercicio

--- prueba2.py
+++ prueba2.py
@@ -4,4 +4,4 @@
 #git commit -m "comentario" #guarda el codigo con el mensaje explicativo; le pones para donde va el paquete
 #git push #subir el codigo; ir a correos y dejar el paquete
 
-print("Hola caracola puchinbola")
+print("Hola caracola puchinbola...")
